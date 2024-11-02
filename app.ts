359bf8c87c903eb5b40c189cf0f5f2ebc9816f02
Atul Tiwari <tiwariatul9526@gmail.com>
2024-11-02 22:17:16 +0530

    annotation type

--- app.ts
+++ app.ts
@@ -1,55 +1,62 @@
-//primitive types
+// //primitive types
 
-let a = 12; //number
-let b = true; //boolean
-let c = "Moti"; //string
+// let a = 12; //number
+// let b = true; //boolean
+// let c = "Moti"; //string
 
-console.log(c);
+// console.log(c);
 
-let arr = []; //blank array
-const abc = [1, 2, 3, 4]; //fill array
+// let arr = []; //blank array
+// const abc = [1, 2, 3, 4]; //fill array
 
-//tuples
-let data: [number, string] = [89, "Moti"];
+// //tuples
+// let data: [number, string] = [89, "Moti"];
 
-//enums
+// //enums
 
-enum UserData {
-  ADMIN = "admin",
-  GUEST = "guest",
-  SUPER_ADMIN = "super_admin",
+// enum UserData {
+//   ADMIN = "admin",
+//   GUEST = "guest",
+//   SUPER_ADMIN = "super_admin",
+// }
+
+// enum StatusCode {
+//   ABANDONED = "kalua kalua kalua 500",
+//   NOT_FOUND = "404 not_found",
+// }
+
+// StatusCode.NOT_FOUND;
+
+// //any , unknown
+// // let d;
+
+// // let a: unknown;
+// // a = 12;
+// // a = "Moti";
+
+// let z: unknown;
+
+// z = 12;
+// z = "Moti";
+
+// if (typeof z == "string") z.toUpperCase();
+
+// //void
+// function xyz(): void {
+//   console.log("Your name is Kalua");
+// }
+
+// //never
+// function zyx(): never {
+//   while (true) {}
+// }
+
+// zyx();
+// console.log("hello");
+
+function data(a: number, b: string): void {
+  a = 12;
+  b = "Kalua";
 }
 
-enum StatusCode {
-  ABANDONED = "kalua kalua kalua 500",
-  NOT_FOUND = "404 not_found",
-}
-
-StatusCode.NOT_FOUND;
-
-//any , unknown
-// let d;
-
-// let a: unknown;
-// a = 12;
-// a = "Moti";
-
-let z: unknown;
-
-z = 12;
-z = "Moti";
-
-if (typeof z == "string") z.toUpperCase();
-
-//void
-function xyz(): void {
-  console.log("Your name is Kalua");
-}
-
-//never
-function zyx(): never {
-  while (true) {}
-}
-
-zyx();
-console.log("hello");
+console.log(data);
